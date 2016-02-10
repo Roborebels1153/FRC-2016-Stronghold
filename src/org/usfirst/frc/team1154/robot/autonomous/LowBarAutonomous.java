d2e629a71a28598037a48022dcc421cccfd0f5fb
Walpole Robotics <roborebels1153@gmail.com>
2016-02-09 19:50:55 -0500

    Creating first auto mode
    added 1st class

---
+++ src/org/usfirst/frc/team1154/robot/autonomous/LowBarAutonomous.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team1154.robot.autonomous;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class LowBarAutonomous extends CommandGroup{
+	public LowBarAutonomous() {
+		
+	}
+
+}
